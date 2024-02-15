a828419995ff729170eefcbc81379b8959354fef
rinklepoonia <rinklepoonia889@gmail.com>
2024-02-15 16:54:47 +0530

    menu-icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,9 @@
 const menuIcon = document.querySelector(".menuIcon")
 const menulist = document.querySelector(".menulist")
 
+
 menuIcon.addEventListener("click", function () {
     menulist.classList.toggle("show");
     document.body.classList.toggle("overFlowHidden")
+    document.querySelector(".menuIcon").classList.toggle("cross")
 })
