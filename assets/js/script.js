63360c24aab90ffd674bc048f18f856619bda21e
rinklepoonia <rinklepoonia889@gmail.com>
2024-02-15 16:47:27 +0530

    company

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// function openNav() {
+//     document.querySelector(".menuList").classList.toggle(".show");
+//     document.body.classList.toggle("overflow-hidden");
+//     document.querySelector(".menuIcon").classList.toggle("cross")
+// }
+const menuIcon = document.querySelector(".menuIcon")
+const menulist = document.querySelector(".menulist")
+
+menuIcon.addEventListener("click", function () {
+    menulist.classList.toggle("show");
+    document.body.classList.toggle("overFlowHidden")
+})
